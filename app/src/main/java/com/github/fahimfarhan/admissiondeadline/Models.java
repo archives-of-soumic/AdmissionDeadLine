ca00154e522e0f9814f239f63ffdc1da62696fbe
fahimfarhan <fahim.farhan@outlook.com>
2018-10-05 00:41:34 +0600

    FC dev 5

--- app/src/main/java/com/github/fahimfarhan/admissiondeadline/Models.java
+++ app/src/main/java/com/github/fahimfarhan/admissiondeadline/Models.java
@@ -1,6 +1,13 @@
 package com.github.fahimfarhan.admissiondeadline;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 public class Models {
-    static String site;
+    static String site, exam, email;
+    static FirebaseUser user;
+    static DatabaseReference databaseReference;
+    static FirebaseAuth auth;
     public Models(){}
 }
